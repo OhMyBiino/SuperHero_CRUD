b7eea8deabf87e0c385e694450f85bb7584a1ef5
ohmybiino <davenarzobal@gmail.com>
2025-11-11 23:43:33 +0800

    Configure HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   title = 'SuperHero_CRUD';
+
   heroes:SuperHero[] = [];
 
   constructor(private superHeroService:SuperHeroService){}
