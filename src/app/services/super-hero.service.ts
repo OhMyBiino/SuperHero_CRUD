8130deff2dd237040396ab8c294c2c423a691f01
ohmybiino <davenarzobal@gmail.com>
2025-11-12 21:09:04 +0800

    Inject HttpClient to SuperHero Service

--- src/app/services/super-hero.service.ts
+++ src/app/services/super-hero.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { SuperHero } from '../models/super-hero';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SuperHeroService {
   
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   public getSuperHeroes(): SuperHero[]
   {
