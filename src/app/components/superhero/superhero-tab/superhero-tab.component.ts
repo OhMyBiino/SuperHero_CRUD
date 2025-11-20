72d2f8aefa4c12460e7b84e8cbad9b183d97e68d
ohmybiino <davenarzobal@gmail.com>
2025-11-20 14:10:52 +0800

    Add superhero-tab component

---
+++ src/app/components/superhero/superhero-tab/superhero-tab.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-superhero-tab',
+  imports: [],
+  templateUrl: './superhero-tab.component.html',
+  styles: ``
+})
+export class SuperheroTabComponent {
+
+}
