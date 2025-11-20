6e869cf17510eca84ba203c39455310c453751c2
ohmybiino <davenarzobal@gmail.com>
2025-11-20 17:54:04 +0800

    Add default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SuperheroTabComponent } from './components/superhero/superhero-tab/superhero-tab.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: AppComponent},
+    {path: "SuperHeroes", component: SuperheroTabComponent}
+];
--- src/app/components/superhero/superhero-tab/superhero-tab.component.ts
+++ src/app/components/superhero/superhero-tab/superhero-tab.component.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SuperHero } from '../../../models/super-hero';
+import { SuperHeroService } from '../../../services/super-hero.service';
 
 @Component({
   selector: 'app-superhero-tab',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './superhero-tab.component.html',
   styles: ``
 })
 export class SuperheroTabComponent {
+  superHeroes: SuperHero[] = [];
 
+  constructor(private superHeroService: SuperHeroService){}
+  
+  ngOnInit(): void
+  {
+    this.superHeroService
+    .getSuperHeroes()
+    .subscribe(result => { this.superHeroes = result });
+    console.log(this.superHeroes)
+    console.log("hey");
+  }
 }
